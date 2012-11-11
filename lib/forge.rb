7305878790c462e6a4ede93716cb54f6634ad4be
Matte Noble <me@mattenoble.com>
2012-11-11 10:48:50 -0500

    Define, build and create. Initial commit

---
+++ lib/forge.rb
@@ -0,0 +1,71 @@
+require "forge/version"
+
+module Forge
+  class ForgeError < StandardError
+  end
+
+  class DuplicateFactoryError < ForgeError
+  end
+
+  class MissingFactoryError < ForgeError
+  end
+
+  class << self
+    attr_accessor :registry
+  end
+
+  def self.reset!
+    @registry = {}
+  end
+
+  def self.define(name, klass, &block)
+    @registry ||= {}
+    raise DuplicateFactoryError if @registry[name]
+    @registry[name] = Definition.new(klass, &block)
+  end
+
+  def self.build(name, attrs={})
+    raise MissingFactoryError unless @registry[name]
+    @registry[name].build(attrs)
+  end
+
+  def self.create(name, attrs={})
+    raise MissingFactoryError unless @registry[name]
+    @registry[name].create(attrs)
+  end
+
+  class Definition
+    attr_accessor :klass, :block
+
+    def initialize(klass, &block)
+      @klass = klass
+      @block = block
+    end
+
+    def build(attrs={})
+      instance = @klass.new.tap { |o| @block.call(o) }
+      attrs.each { |k,v| instance.public_send(:"#{k}=", v) }
+      instance
+    end
+
+    def create(attrs={})
+      build(attrs).tap { |o| o.save }
+    end
+  end
+
+  module DSL
+    def define(*args, &block)
+      Forge.define(*args, &block)
+    end
+
+    def build(*args)
+      Forge.build(*args)
+    end
+
+    alias_method :forge, :build
+
+    def create(*args)
+      Forge.create(*args)
+    end
+  end
+end
---
+++ spec/forge_spec.rb
@@ -0,0 +1,48 @@
+require "ostruct"
+require_relative "../lib/forge"
+
+describe Forge do
+  class User < OpenStruct; end
+
+  before do
+    Forge.reset!
+    Forge.define(:user, User) do |u|
+      u.name = "Spike"
+      u.location = "Mars"
+    end
+  end
+
+  it "saves factory definitions to a registry" do
+    Forge.registry[:user].should_not be_nil
+  end
+
+  it "raises an error when defining the same factory twice" do
+    expect { Forge.define(:user, User) }.to raise_error(Forge::DuplicateFactoryError)
+  end
+
+  it "builds a forged object of the type specified when defining it" do
+    Forge.build(:user).should be_a User
+  end
+
+  it "sets attributes on the forged objects based on the block passed during definition" do
+    Forge.build(:user).name.should == "Spike"
+  end
+
+  it "allows attributes to be overwritten via build" do
+    Forge.build(:user, name: "Jet").name.should == "Jet"
+  end
+
+  it "raises an error when trying to build a non-existent factory" do
+    expect { Forge.build(:not_a_thing) }.to raise_error(Forge::MissingFactoryError)
+  end
+
+  it "creates factories with an attribute that is another factory to be built" do
+    Forge.define(:bounty_hunter, User) do |f|
+      f.name = "Spike"
+      f.location = "Mars"
+      f.partner = Forge.build(:user, name: "Jet")
+    end
+
+    Forge.build(:bounty_hunter).partner.name.should == "Jet"
+  end
+end
